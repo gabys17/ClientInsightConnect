176744efbbcd87e56d214c711e1eeb66dfbd3f58
Gabriela Silva <gvsde8@gmail.com>
2024-03-12 18:01:31 +0000

    calendar done

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,41 +4,46 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import agendaService from "../services/agenda.service";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { Link } from "react-router-dom/dist";
-import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 function DemoApp() {
+  const navigate = useNavigate();
   const [events, setEvents] = useState([]);
 
   async function fetchAgendas() {
     try {
       const response = await agendaService.getAllAgendas();
-      setEvents(response.data);
-      console.log(response.data)
-
-      } catch (error) {
+      const mappedEvents = response.data.map((agenda) => ({
+        title: agenda.title,
+        start: new Date(agenda.start_time), // Assuming start_time is in ISO format
+        end: new Date(agenda.end_time), // Assuming end_time is in ISO format
+        description: agenda.description,
+        owner: agenda.owner,
+        participants: agenda.participants,
+        id: agenda._id // Assuming _id is the unique identifier of the event
+        // Add other properties as needed
+      }));
+      setEvents(mappedEvents);
+    } catch (error) {
       console.error("Error fetching agendas:", error);
-      return [];
     }
   }
 
   useEffect(() => {
     async function loadEvents() {
-/*       const fetchedEvents = await fetchAgendas();
-      setEvents(fetchedEvents); */
       await fetchAgendas();
     }
-
     loadEvents();
   }, []);
 
-  const handleDateClick = () => {
-     <Link to="/agenda/:id"><Button change="blue">Create appointment</Button></Link>;
+  const handleDateClick = (arg) => {
+    navigate("/agenda/create", { state: { date: arg.date } });
   };
 
-/*   function handleEvents(events) {
-    setCurrentEvents(events);
-  } */
+  const handleEventClick = (eventClickInfo) => {
+    const eventId = eventClickInfo.event.extendedProps.id;
+    navigate(`/agenda/${eventId}`);
+  };
 
   return (
     <div>
@@ -53,24 +58,25 @@ function DemoApp() {
         eventContent={renderEventContent}
         events={events}
         select={handleDateClick}
-
+        eventClick={handleEventClick}
       />
     </div>
-  )}
-  // a custom render function
-  function renderEventContent(eventInfo) {
-    /*const { start_time, end_time } = eventInfo.event.extendedProps.when;*/ 
-     return (
-      <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-        <i>{eventInfo.event.description}</i>
-        <i>{eventInfo.event.owner}</i>
-        <i>{eventInfo.event.participants}</i>
-        <i>{eventInfo.event.start_time}</i>
-        <i>{eventInfo.event.end_time}</i>
-      </>
-    );
-  }
+  );
+}
 
-  export default DemoApp;
+// Custom render function for event content
+function renderEventContent(eventInfo) {
+  const { title, description, owner, participants } = eventInfo.event.extendedProps;
+
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{title}</i>
+      <i>{description}</i>
+      <i>{owner}</i>
+      <i>{participants}</i>
+    </>
+  );
+}
+
+export default DemoApp;
